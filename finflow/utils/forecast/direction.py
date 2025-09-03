1508d4854854e617917fd74bd1a1a366ddebe1d1
Etienne <etienne.micheladj@gmail.com>
2025-09-03 17:26:45 +0200

    minor

--- finflow/utils/forecast/direction.py
+++ finflow/utils/forecast/direction.py
@@ -3,4 +3,4 @@ from ..connections.database import Database as ConnectionObject
 
 async def set_forecast_direction(data, data_length=None):
     conn = ConnectionObject()
-    conn.set_data(data, table_name=f"forecast_direction", data_length=data_length)
+    conn.save_dataframe(pd.DataFrame(data), table_name=f"forecast_direction", data_length=data_length)
