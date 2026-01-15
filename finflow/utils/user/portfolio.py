9c63c8e94e121dd42bee069d30b002f67bf41eb5
Etienne <etienne.micheladj@gmail.com>
2026-01-15 16:57:59 +0100

    minoir fix

--- finflow/utils/user/portfolio.py
+++ finflow/utils/user/portfolio.py
@@ -1,9 +1,12 @@
 from ..connections.database import Database as ConnectionObject 
-import time
+import datetime as dt
+
 
 async def set_portfolio(data):
     conn = ConnectionObject()
-    now = int(str(int(time.time())) + "000")
+    today = dt.datetime.now()
+    today = dt.datetime(year=today.year, month=today.month, day=today.day, hour=today.hour, minute=today.minute)
+    now = int(str(int(today.timestamp())) + "000")
     if len(data["positions"]) > 0:
         for i in range(len(data["positions"])): data["positions"][i]["timestamp"] = now
         conn.upload_data(data["positions"], "portfolio_positions", conflict_cols=["order_id"])
